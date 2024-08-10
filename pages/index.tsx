dcff8389ed74329c4b0ea7261e8545136626b49a
parkksss <yesleee9211@gmail.com>
2024-08-10 21:51:36 +0900

    feat: 로그인 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,41 @@ import NextLink from 'next/link'
 import { ButtonGroup } from '@/components/button_group';
 import { NextPage } from 'next';
 import { Box, Center, Flex, Heading, Input, Link } from '@chakra-ui/react';
+import { ChangeEvent, useState } from 'react';
+import { login } from './api/user';
+import { useRouter } from 'next/router';
 
 const IndexPage: NextPage = () => {
+    const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onEailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target as any;
+        setEmail(value);
+    }
+    const onPasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target as any;
+        setPassword(value);
+    }
+    
+    const handleLogin = async (): Promise<void> => {
+        if (!email.includes('@') || password.length < 8) {
+            alert("이메일과 비밀번호가 올바르지 않습니다.");
+            return;
+        };
+        try {
+            const res = await login({email, password});
+            console.log("서버 응답 데이터 : ", res);
+            const accessTokken = res.data.accessToken;
+            localStorage.setItem('accessToken', accessTokken);
+            router.push('/main');
+        } catch (error) {
+            console.log('로그인 실패 : ', error);
+        }
+        return;
+    }
+
     return <ServiceLayout title="캘린더스">
         <Box maxW="md" mx="auto" pt="200">
             <Flex justify="center">
@@ -12,11 +45,14 @@ const IndexPage: NextPage = () => {
             </Flex>
         </Box>
         <Box maxW="md" mx="auto" pt="6">
-            <Input variant='flushed' placeholder="이메일" />
-            <Input variant='flushed' placeholder="비밀번호" />
+            <Input variant='flushed' placeholder="이메일" 
+                        type="text"
+                        value={email}
+                        onChange={onEailHandler}/>
+            <Input variant='flushed' placeholder="비밀번호" type="password" onChange={onPasswordHandler} />
         </Box>
         <Box maxW="md" mx="auto" pt="6">
-            <ButtonGroup title="로그인" ftColor="white" bgColor="#3589F0" onClick={() => {}}/>
+            <ButtonGroup title="로그인" ftColor="white" bgColor="#3589F0" onClick={handleLogin}/>
             <Flex justify="center" pt="4">
                 <Link as={NextLink} href='/register' fontSize="sm" color="gray.600">회원가입</Link>
             </Flex>
---
+++ pages/main.tsx
@@ -0,0 +1,17 @@
+import { ServiceLayout } from '@/components/service_layout';
+import NextLink from 'next/link'
+import { Box, Heading, Input, Flex, Text, Link } from '@chakra-ui/react';
+import { ButtonGroup } from '@/components/button_group';
+import React, { ChangeEvent, useCallback, useState } from 'react'
+
+const Main = () => {
+    return (
+        <ServiceLayout title="캘린더스_메인">
+            <Box maxW="md" mx="auto" pt="20">
+                메인 페이지
+            </Box>
+        </ServiceLayout>
+    )
+};
+
+export default Main;
